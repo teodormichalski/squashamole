e38f7716ac024d9cf6c48f92f5a7f95a6b87b1e3
Teodor Michalski <teodor.michalski@gmail.com>
2019-07-20 18:00:10 +0200

    Mood changes.

--- Assets/Misbehaviour.cs
+++ Assets/Misbehaviour.cs
@@ -10,6 +10,14 @@ public class Misbehaviour : MonoBehaviour
 	public int damage = 0;
 	public bool dead = false;
 	public bool receive;
+	public int mood = 1;
+
+	public GameObject[] moodFeatures1;
+	public GameObject[] moodFeatures2;
+	public GameObject[] moodFeatures3;
+	public GameObject[] moodFeatures4;
+	public GameObject[] moodFeatures5;
+	private GameObject[][] moodFeatures;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +29,14 @@ public class Misbehaviour : MonoBehaviour
         	scarsOn[i] = false;
         	scars[i].GetComponent<SpriteRenderer>().sortingOrder = -10;
         }
+        mood = 1;
+        moodFeatures = new GameObject[5][];
+        moodFeatures[0] = moodFeatures1;
+        moodFeatures[1] = moodFeatures2;
+        moodFeatures[2] = moodFeatures3;
+        moodFeatures[3] = moodFeatures4;
+        moodFeatures[4] = moodFeatures5;
+        ChangeMoodFeatures();
     }
 
     // Update is called once per frame
@@ -36,6 +52,11 @@ public class Misbehaviour : MonoBehaviour
     void ReceiveDamage()
     {
     	damage++;
+    	if (damage % 4 == 0)
+    	{
+    		mood++;
+    		ChangeMoodFeatures();
+    	}
     	if (damage > maxDamage) 
     	{
     		dead = true;
@@ -47,5 +68,14 @@ public class Misbehaviour : MonoBehaviour
     	scarsOn[i % maxDamage] = true;
     	scars[i % maxDamage].GetComponent<SpriteRenderer>().sortingOrder = 2;
 
+    }
+
+    void ChangeMoodFeatures() 
+    {
+    	foreach (GameObject[] features in moodFeatures)
+    		foreach (GameObject f in features)
+    			f.SetActive(false);
+    	foreach (GameObject f in moodFeatures[mood - 1])
+    		f.SetActive(true);
     }
 }
